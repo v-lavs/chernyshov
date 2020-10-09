341a6b5e35ef3cb81bc38904ed15f7adb3431eb3
v-lavs <lavskavanda@gmail.com>
2020-10-09 19:59:54 +0300

    123233

--- src/js/main.js
+++ src/js/main.js
@@ -4,36 +4,9 @@
 
 //= include ../../node_modules/jquery/dist/jquery.js ;
 //= include ./swiper-bundle.esm.browser.min.js ;
+//= include ./jquery.waypoints.min.js ;
 
-function isElementInViewport(el) {
-    if (el instanceof jQuery) {
-        el = el[0];
-    }
-    var rect = el.getBoundingClientRect();
-    return (
-        (rect.top <= 0
-            && rect.bottom >= 0)
-        ||
-        (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.top <= (window.innerHeight || document.documentElement.clientHeight))
-        ||
-        (rect.top >= 0 &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
-    );
-}
 
-$.fn.isInViewport = function () {
-    var top_of_element = $(this).offset().top;
-    var bottom_of_element = $(this).offset().top + $(this).outerHeight();
-    var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
-    var top_of_screen = $(window).scrollTop();
-
-    if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
-        // the element is visible, do something
-    } else {
-        // the element is not visible, do something else
-    }
-};
 
 function scrollToAnchor(el, time) {
     $('.content').animate({scrollTop: $(el).get(0).offsetTop}, time);
@@ -43,13 +16,29 @@ let scrolling = false;
 const scrollingTime = 1000;
 
 $(document).ready(function () {
+
+    var waypoints = $('.js-detect').waypoint(function(direction) {
+       const $el = $(this.element);
+       const delay = $el.data('delay') || 0;
+       setTimeout(function () {
+           $el.addClass('active');
+       }, +delay);
+    }, {
+        offset: '75%'
+    });
+
+
+
     const slideClass = 'content__slide';
-    window.addEventListener('wheel', function (e) {
+
+    document.querySelector('.content').addEventListener('wheel', function (e) {
+
+        const scrollTop = document.querySelector('.content').scrollTop;
+
         if (!scrolling) {
-            scrolling = true;
             const section = $(e.target).hasClass(slideClass) ? e.target : $(e.target).parents('.' + slideClass);
             let nextSection;
-            if(e.deltaY > 0) {
+            if (e.deltaY > 0) {
                 const nextEl = $(section).next('.' + slideClass);
                 nextSection = nextEl.length > 0 ? nextEl : $('.footer');
             } else {
@@ -57,10 +46,12 @@ $(document).ready(function () {
             }
 
             if (nextSection.length > 0) {
-                scrollToAnchor(nextSection, scrollingTime)
-                setTimeout(function () {
-                    scrolling = false;
-                }, scrollingTime)
+                // scrolling = true;
+                // scrollToAnchor(nextSection, scrollingTime);
+                //
+                // setTimeout(function () {
+                //     scrolling = false;
+                // }, scrollingTime)
             }
         }
     });
