642652825ada995ff1baf74581c9c19a29683224
v-lavs <lavskavanda@gmail.com>
2020-10-12 09:13:06 +0300

    fixed footer

--- dist/scripts.js
+++ dist/scripts.js
@@ -21021,7 +21021,7 @@ $(document).ready(function () {
         // scrollbars: false,
         touchScroll: true,
         interstitialSection: ".footer",
-        after: function (currIndex, sections) {
+        before: function (currIndex, sections) {
             sections.forEach(function (section) {
                 section.removeClass('section_active')
             });
