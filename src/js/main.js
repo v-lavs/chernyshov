d9baa64833d2f2cd5e1c5526adc3a14c6ecdb6ce
v-lavs <lavskavanda@gmail.com>
2020-10-14 10:33:03 +0300

    add title to arrows

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,33 @@
 //= include ./swiper-bundle.esm.browser.min.js ;
 //= include ./scrollify.js ;
 
+// CUSTOM SCRIPTS
+
 $(document).ready(function () {
+    //SCROLLIfY
+    $.scrollify({
+        section: ".content__slide",
+        scrollSpeed: 1000,
+        offset: 0,
+        touchScroll: false,
+        interstitialSection: ".footer",
+        before: function (currIndex, sections) {
+            sections.forEach(function (section) {
+                section.removeClass('section_active')
+            });
+            sections[currIndex].addClass('section_active');
+        }
+    });
+
+    if ($(window.innerWidth) <= 1080) {
+        if (!$.scrollify.isDisabled()) {
+            $.scrollify.disable();
+        }
+    } else {
+        if ($.scrollify.isDisabled()) {
+            $.scrollify.enable();
+        }
+    }
 
     $('.nav-services li').each(function (i, item) {
         $(item).css({animationDelay: (i + 1) * 100 + 200 + 'ms'})
@@ -36,6 +62,24 @@ $(document).ready(function () {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
+    });
+
+    function getArrowTitles() {
+        const slides = $('.service-slider .swiper-slide');
+        const prevSlide = slides.get(swiper.previousIndex);
+        const nextSlide = slides.get(swiper.activeIndex + 1);
+
+        $('.sticky-prev-name').html(swiper.activeIndex > 0 ? $(prevSlide).find('.section__title').text() : '');
+        $('.sticky-next-name').html($(nextSlide).find('.section__title').text() || '');
+
+        $('.sticky-prev-name').html(swiper.activeIndex > 0 ? $(prevSlide).find('.section__title').text() : '');
+        $('.sticky-next-name').html($(nextSlide).find('.section__title').text() || '');
+    }
+
+    getArrowTitles();
+
+    swiper.on('slideChange', function () {
+        getArrowTitles();
     });
 
     $('.nav-services a').click(function (e) {
@@ -74,29 +118,4 @@ $(document).ready(function () {
             $(this).removeClass('has_value');
         }
     });
-
-    //SCROLLIfY
-    const scroll = $.scrollify({
-        section: ".content__slide",
-        scrollSpeed: 1000,
-        offset: 0,
-        touchScroll: false,
-        interstitialSection: ".footer",
-        before: function (currIndex, sections) {
-            sections.forEach(function (section) {
-                section.removeClass('section_active')
-            });
-            sections[currIndex].addClass('section_active');
-        }
-    });
-
-    if ($(window.innerWidth <= 1080)) {
-        if (!$.scrollify.isDisabled()) {
-            scroll.disable();
-        }
-    } else {
-        if ($.scrollify.isDisabled()) {
-            scroll.enable();
-        }
-    }
 });
